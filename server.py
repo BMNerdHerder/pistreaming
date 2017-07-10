8bfb9cf711dfc856ec9e0ec8646ae6e844bb2cd6
BMNerdHerder <bmnerdherder@icloud.com>
2017-07-10 11:47:41 +0100

    1

--- server.py
+++ server.py
@@ -145,7 +145,7 @@ def main():
         output = BroadcastOutput(camera)
         broadcast_thread = BroadcastThread(output.converter, websocket_server)
         print('Starting recording')
-        camera.start_recording(output, 'yuv')
+        camera.start_recording(output, 'yuv', resize=(320, 240))
 
         try:
             print('Starting websockets thread')
