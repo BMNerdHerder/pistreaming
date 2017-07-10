2bb7651e069c30ae921c56395551613a63a821e4
BMNerdHerder <bmnerdherder@icloud.com>
2017-07-10 12:33:16 +0100

    RC01

---
+++ Install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install python-picamera libav-tools git python3-picamera python3-ws4py -y
+
+sudo chmod +x server.py
+sudo chmod +x server-service.sh
+
+sudo cp pistreaming.service /lib/systemd/system/pistreaming.service
+
+sudo systemctl enable pistreaming.service
+sudo systemctl start pistreaming.service
+sudo systemctl status pistreaming.service
